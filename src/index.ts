fc1130a4a5f6a00a14dd323716a068e7700737b8
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-01-23 15:05:24 -0500

    Improved sentry logging
    Added sentry tags tags and now capturing exceptions.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import * as Sentry from "@sentry/node"
 import * as Tracing from "@sentry/tracing"
 import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
+import { platform, version, release } from 'os';
+import { inspect } from 'util';
 
 import { api } from './api/server'
-import { i18nextLogger } from "./utils";
+import { i18nextLogger, logger } from "./utils";
 
 
 // init internationalization
@@ -34,6 +36,10 @@ i18next.use(Backend).use(i18nextLogger).init({
 // init sentry
 Sentry.init({
     dsn: "",
+    release: `wikipedia-bot-canary@${process.env.npm_package_version}`,
+    environment: process.env.NODE_ENV || 'dev',
+    maxBreadcrumbs: 100,
+    serverName: process.env.serverName || 'dev',
 
     integrations: [
         // enable HTTP calls tracing
@@ -71,6 +77,12 @@ Sentry.init({
     },
 });
 
+// set sentry tags
+Sentry.setTag('platform', platform());
+Sentry.setTag('os.name', version());
+Sentry.setTag('os', version() + ' ' + release());
+Sentry.setTag('node', process.version);
+
 const startServer = Sentry.startTransaction({
     op: "start api",
     name: "Started the api server",
@@ -80,6 +92,38 @@ try {
     const server = new api(3420, 'INSTANCE001')
 } catch (error) {
     Sentry.captureException(error);
+    logger.error(inspect(error));
 } finally {
     startServer.finish()
 }
+
+// Capture unhandledRejections
+process.on('unhandledRejection', (error: Error) => {
+        // Log
+        logger.error(`Unhandled Rejection: ${inspect(error)}`);
+        // Send to sentry
+        Sentry.captureException(error);
+});
+
+// Capture uncaughtExceptionMonitors
+process.on(
+    'uncaughtExceptionMonitor',
+    (error: Error, origin: string) => {
+        // Log
+        logger.error(
+            `Uncaught Exception Monitor: ${inspect(
+                error,
+            )}\n${origin}`,
+        );
+        // Send to sentry
+        Sentry.captureException(error);
+    },
+);
+
+// Capture warnings
+process.on('warning', (warning: Error) => {
+    // Log
+    logger.warn(`Warning: ${inspect(warning)}`);
+    // Send to sentry
+    Sentry.captureException(warning);
+});
