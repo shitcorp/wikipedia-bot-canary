2254ee3bbdc95acec187148d33eb1a1c7e8cb530
MeerBiene <b.buhles@gmail.com>
2021-01-23 21:35:28 +0100

    added tweetnacl

--- src/api/routes/api/v1/interaction.ts
+++ src/api/routes/api/v1/interaction.ts
@@ -1,9 +1,29 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import tweetnacl from "tweetnacl";
 
 export const route = {
-    name: 'interaction',
-    method: 'POST',
-    route: async (req:Request, res:Response, commands:Map<String, any>):Promise<void> => {
+    name: "interaction",
+    method: "POST",
+    route: async (
+        req: any,
+        res: Response,
+        commands: Map<String, any>
+    ): Promise<void> => {
+        // Your public key can be found on your application in the Developer Portal
+        const PUBLIC_KEY = "APPLICATION_PUBLIC_KEY";
 
-    }
+        const signature = req.get("X-Signature-Ed25519");
+        const timestamp = req.get("X-Signature-Timestamp");
+        const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
+
+        const isVerified = tweetnacl.sign.detached.verify(
+            Buffer.from(timestamp + body),
+            Buffer.from(signature, "hex"),
+            Buffer.from(PUBLIC_KEY, "hex")
+        );
+
+        if (!isVerified) {
+            return res.status(401).end("invalid request signature");
+        }
+    },
 };
---
+++ src/models/config.ts
@@ -0,0 +1,6 @@
+export interface config{
+    _id: String,
+    lang: String,
+    channelblacklist: String[]
+    
+}
