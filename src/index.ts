430f98dd6ba2f105bb4fca8c6b91e06d4d9e314c
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-01-24 12:43:07 -0500

    fix(compile errors): fixed wide range of compile errors (#11)
    Added eslint and prettier for formating and linting. Removed body-parser as it was redundant. Fixed
    a good chunk of the compile errors, but a great deal still remain. Replaced require statements with
    imports, if they were not entirely removed. Added more sentry exception capturing to try catch
    blocks. Reworked some logger imports to import from the utils index file and not the logger util
    file.

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,85 @@
-require('dotenv').config()
+(async () => {
+  (await import('dotenv')).config();
+})();
 
-import * as Sentry from "@sentry/node"
-import * as Tracing from "@sentry/tracing"
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 import { platform, version, release } from 'os';
 import { inspect } from 'util';
 
-import { api } from './api/server'
-import { i18nextLogger, logger } from "./utils";
-
+import { api } from './api/server';
+import { i18nextLogger, logger } from './utils';
 
 // init internationalization
-i18next.use(Backend).use(i18nextLogger).init({
+i18next
+  .use(Backend)
+  .use(i18nextLogger)
+  .init({
     lng: 'en',
     fallbackLng: 'en',
     preload: ['en'],
     ns: ['translation'],
     defaultNS: 'translation',
     backend: {
-        // path where resources get loaded from, or a function
-        // returning a path:
-        // function(lngs, namespaces) { return customPath; }
-        // the returned path will interpolate lng, ns if provided like giving a static path
-        loadPath: 'src/locales/{{lng}}/{{ns}}.json',
+      // path where resources get loaded from, or a function
+      // returning a path:
+      // function(lngs, namespaces) { return customPath; }
+      // the returned path will interpolate lng, ns if provided like giving a static path
+      loadPath: 'src/locales/{{lng}}/{{ns}}.json',
 
-        // path to post missing resources
-        addPath: 'src/locales/{{lng}}/{{ns}}.missing.json'
+      // path to post missing resources
+      addPath: 'src/locales/{{lng}}/{{ns}}.missing.json',
     },
     debug: false,
-})
+  });
 
 // specify lang ex:
 // t('welcome', { lng: 'en' })
 
-
 // init sentry
 Sentry.init({
-    dsn: "",
-    release: `wikipedia-bot-canary@${process.env.npm_package_version}`,
-    environment: process.env.NODE_ENV || 'dev',
-    maxBreadcrumbs: 100,
-    serverName: process.env.SERVER_NAME || 'dev',
+  dsn: '',
+  release: `wikipedia-bot-canary@${process.env.npm_package_version}`,
+  environment: process.env.NODE_ENV || 'dev',
+  maxBreadcrumbs: 100,
+  serverName: process.env.SERVER_NAME || 'dev',
 
-    integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Tracing.Integrations.Express(),
-    ],
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express(),
+  ],
 
-    // clean any possibly present sensitive user data
-    beforeSend(event) {
-        // if user
-        if (event.user) {
-            // scrub any possible sensitive data
+  // clean any possibly present sensitive user data
+  beforeSend(event) {
+    // if user
+    if (event.user) {
+      // scrub any possible sensitive data
 
-            // don't send email address
-            delete event.user.email;
-            // don't send username
-            delete event.user.username;
-            // don't send ip address
-            delete event.user.ip_address;
-        }
-        return event;
-    },
+      // don't send email address
+      delete event.user.email;
+      // don't send username
+      delete event.user.username;
+      // don't send ip address
+      delete event.user.ip_address;
+    }
+    return event;
+  },
 
-    // how much of each `event` we want
-    tracesSampler: (samplingContext) => {
-        switch (samplingContext.transactionContext.op) {
-            case 'start api':
-                return 1;
-            case 'shutdown':
-                return 1;
-            default:
-                return 0.5;
-        }
-    },
+  // how much of each `event` we want
+  tracesSampler: (samplingContext) => {
+    switch (samplingContext.transactionContext.op) {
+      case 'start api':
+        return 1;
+      case 'shutdown':
+        return 1;
+      default:
+        return 0.5;
+    }
+  },
 });
 
 // set sentry tags
@@ -86,48 +89,47 @@ Sentry.setTag('os', version() + ' ' + release());
 Sentry.setTag('node', process.version);
 
 const startServer = Sentry.startTransaction({
-    op: "start api",
-    name: "Started the api server",
+  op: 'start api',
+  name: 'Started the api server',
 });
 
 try {
-    new api(3420, 'INSTANCE001')
-
+  new api(3420, 'INSTANCE001');
 } catch (error) {
-    console.log(error)
-    Sentry.captureException(error);
-    logger.error(inspect(error));
+  console.log(error);
+  Sentry.captureException(error);
+  logger.error(inspect(error));
 } finally {
-    startServer.finish()
+  startServer.finish();
 }
 
 // Capture unhandledRejections
 process.on('unhandledRejection', (error: Error) => {
-        // Log
-        logger.error(`Unhandled Rejection: ${inspect(error)}`);
-        // Send to sentry
-        Sentry.captureException(error);
+  // Log
+  logger.error(`Unhandled Rejection: ${inspect(error)}`);
+  // Send to sentry
+  Sentry.captureException(error);
 });
 
 // Capture uncaughtExceptionMonitors
 process.on(
-    'uncaughtExceptionMonitor',
-    (error: Error, origin: string) => {
-        // Log
-        logger.error(
-            `Uncaught Exception Monitor: ${inspect(
-                error,
-            )}\n${origin}`,
-        );
-        // Send to sentry
-        Sentry.captureException(error);
-    },
+  'uncaughtExceptionMonitor',
+  (error: Error, origin: string) => {
+    // Log
+    logger.error(
+      `Uncaught Exception Monitor: ${inspect(
+        error,
+      )}\n${origin}`,
+    );
+    // Send to sentry
+    Sentry.captureException(error);
+  },
 );
 
 // Capture warnings
 process.on('warning', (warning: Error) => {
-    // Log
-    logger.warn(`Warning: ${inspect(warning)}`);
-    // Send to sentry
-    Sentry.captureException(warning);
+  // Log
+  logger.warn(`Warning: ${inspect(warning)}`);
+  // Send to sentry
+  Sentry.captureException(warning);
 });
