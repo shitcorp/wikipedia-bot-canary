3e8c7bacc88b630ed19d3109ced64d74f4ed8a77
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-08-27 16:05:37 -0400

    add universal config stuff

---
+++ src/config/apiURLs.ts
@@ -0,0 +1,18 @@
+export const apiUrl = {
+  // german
+  de: 'https://de.wikipedia.org/w/api.php',
+  // english
+  en: 'https://en.wikipedia.org/w/api.php',
+  // spanish
+  es: 'https://es.wikipedia.org/w/api.php',
+  // french
+  fr: 'https://fr.wikipedia.org/w/api.php',
+  // russian
+  ru: 'https://ru.wikipedia.org/w/api.php',
+  // slovak
+  sl: 'https://sl.wikipedia.org/w/api.php',
+  // turkish
+  tr: 'https://tr.wikipedia.org/w/api.php',
+  // yiddish
+  yi: 'https://yi.wikipedia.org/w/api.php'
+};
---
+++ src/config/embed.ts
@@ -0,0 +1,3 @@
+// embed colors
+export const BLUE = 0xe74c3c;
+export const WHITE = 0xffffff;
