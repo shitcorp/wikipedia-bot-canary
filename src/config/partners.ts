80725706388a781c51cb8d3203fc32e7ae432b16
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-08-29 14:04:13 -0400

    add janus to partners list

--- src/config/partners.ts
+++ src/config/partners.ts
@@ -1,14 +1,14 @@
 export default [
   {
-    name: 'Some Project',
-    author: 'Some authors',
-    description: 'Some description',
-    link: 'some link'
-  },
-  {
     name: 'TODO Bot',
     author: 'MeerBiene',
     description: 'A bot that helps you to manage your TODO list',
     link: 'https://github.com/shitcorp/TODOBOT'
+  },
+  {
+    name: 'Janus',
+    author: 'Huskydog9988',
+    description: 'A fully featured and high performance bot for Star Citizen.',
+    link: 'https://github.com/shitcorp/Janus'
   }
 ];
