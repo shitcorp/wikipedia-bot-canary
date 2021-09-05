1d8b2c48ae959c3a011a8cc18e7ea13b60902cd9
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-09-05 22:34:05 +0200

    start settings command, implement message action

---
+++ src/commands/search-with-wikipedia.ts
@@ -0,0 +1,37 @@
+import { CommandOptionType, SlashCreator, CommandContext } from 'slash-create';
+import { Command } from '../structures/';
+import { Embed, getArticle, logger } from '../utils';
+
+export default class WikiCommand extends Command {
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'search with wikipedia',
+      type: 3,
+      description: 'The normal wiki command used for getting short summaries of something the user searched for.'
+    });
+  }
+
+  async run(ctx: CommandContext) {
+    ctx.defer();
+    console.log(ctx);
+    logger.info(ctx);
+
+    // @ts-ignore
+    const searchTerm = ctx.data.resolved;
+    console.log(searchTerm);
+    return 'Hello world';
+
+    const article = await getArticle(ctx.options.search);
+    const wikiEmbed = new Embed()
+      .setAuthor('Wikipedia Bot')
+      .setDescription(article.summary)
+      .setColor('0099ff')
+      .setURL(article.url);
+
+    if (article.image) wikiEmbed.setThumbnail(article.image);
+
+    return {
+      embeds: [wikiEmbed]
+    };
+  }
+}
