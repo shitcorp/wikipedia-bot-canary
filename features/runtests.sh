f57ed2bd5661a61c37b64675ffc3cfef99bdae1a
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-09-26 10:55:47 +0200

    fix features folder problem

--- features/runtests.sh
+++ features/runtests.sh
@@ -4,7 +4,7 @@
 export NODE_ENV=testing
 
 if [ $CI = "true" ] ; then
-  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: $(date) -->" >> ./features/results/index.html && echo "{ \"files\": [\"index.html\", \"results.tar\"], \"generated\": \"$(date)\", \"generated_unix\": \"$(date +"%s")\" }" > ./features/results/meta.json && tar -czf ./features/results/results.tar ./features/results && rm -rf ./features/results/index.html
+  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: $(date) -->" >> ./features/results/index.html && echo "{ \"files\": [\"index.html\", \"results.tar\"], \"generated\": \"$(date)\", \"generated_unix\": \"$(date +"%s")\" }" > ./features/results/meta.json && tar -czf ./features/results.tar ./features/results && rm -rf ./features/results/index.html && mv ./features/results.tar ./features/results/results.tar
 else
   yarn cucumber
 fi
