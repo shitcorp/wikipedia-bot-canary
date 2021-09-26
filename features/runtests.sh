08ef7342eb529acf4ceaa6ed8cfa6dca41bbe720
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-09-26 10:25:42 +0200

    add timestamp to generated result files

--- features/runtests.sh
+++ features/runtests.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Put your environment variables here
 export NODE_ENV=testing
 
 if [ $CI = "true" ] ; then
-  yarn cucumber > ./features/results/index.html
+  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: ${date} -->" >> ./features/results/index.html && echo "{ \"file\": \"index.html\", \"generated\": \"${date}\" }" > ./features/results/meta.json
 else
   yarn cucumber
 fi
