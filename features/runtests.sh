984c2efe938615d1d31da72d2bb33ebc3f72cefe
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-09-26 10:49:37 +0200

    commit results as tarball

--- features/runtests.sh
+++ features/runtests.sh
@@ -4,7 +4,7 @@
 export NODE_ENV=testing
 
 if [ $CI = "true" ] ; then
-  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: $(date) -->" >> ./features/results/index.html && echo "{ \"file\": \"index.html\", \"generated\": \"$(date)\", \"generated_unix\": \"$(date +"%s")\" }" > ./features/results/meta.json
+  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: $(date) -->" >> ./features/results/index.html && echo "{ \"file\": \"index.html\", \"generated\": \"$(date)\", \"generated_unix\": \"$(date +"%s")\" }" > ./features/results/meta.json && tar -czf ./features/results.tar ./features/results && rm -rf ./features/results/index.html && rm -rf ./features/results/meta.json
 else
   yarn cucumber
 fi
