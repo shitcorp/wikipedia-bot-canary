23c1f2b18fc4ef104dede5b40219568e2f50a574
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-09-26 10:29:32 +0200

    fix date bug

--- features/runtests.sh
+++ features/runtests.sh
@@ -4,7 +4,7 @@
 export NODE_ENV=testing
 
 if [ $CI = "true" ] ; then
-  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: ${date} -->" >> ./features/results/index.html && echo "{ \"file\": \"index.html\", \"generated\": \"${date}\" }" > ./features/results/meta.json
+  yarn cucumber > ./features/results/index.html && echo "<!-- Generated on: $(date) -->" >> ./features/results/index.html && echo "{ \"file\": \"index.html\", \"generated\": \"$(date)\" }" > ./features/results/meta.json
 else
   yarn cucumber
 fi
